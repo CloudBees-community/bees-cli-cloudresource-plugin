b1ccc7b4c53e286dd81c21d957c09650e69fbe2e
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-05-31 11:04:23 -0700

    added a register CRP command

---
+++ src/main/java/org/cloudbees/sdk/plugins/resource/RegisterCRPCommand.java
@@ -0,0 +1,49 @@
+package org.cloudbees.sdk.plugins.resource;
+
+import com.cloudbees.api.cr.CloudResource;
+import com.cloudbees.api.cr.CloudResourceProvider;
+import com.cloudbees.api.cr.CloudResourceProviderRegistry;
+import com.cloudbees.api.oauth.OauthToken;
+import com.cloudbees.api.oauth.TokenRequest;
+import com.cloudbees.sdk.cli.BeesCommand;
+import com.cloudbees.sdk.cli.CLICommand;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+
+import java.net.URL;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+@BeesCommand(group="Resource (New)",description="Register a new CRP")
+@CLICommand("resource2:register")
+public class RegisterCRPCommand extends AbstractResourceCommand {
+    @Argument(index=0,metaVar="CRP_URL",usage="URL of the CRP to register",required=true)
+    URL crp;
+
+    @Option(name="-registry",usage="CRP registry to register this to")
+    URL registry;
+
+    @Override
+    public int main() throws Exception {
+        if (registry==null)
+            registry = new URL("http://crp-registry.apps.cloudbees.com");
+
+        TokenRequest tr = new TokenRequest()
+            .withAccountName(getAccount())
+            .withScope("https://api.cloudbees.com/services/api/subscription/read") // HACK for now
+            .withScope(registry, CloudResourceProviderRegistry.REGISTER_CAPABILITY)
+            .withScope(crp,CloudResource.READ_CAPABILITY)
+            .withGenerateRequestToken(false);
+        OauthToken t = createClient().createToken(tr);
+
+        CloudResource registry = CloudResource.fromOAuthToken(this.registry, t);
+        CloudResource crp      = CloudResource.fromOAuthToken(this.crp,      t);
+
+        registry.coerce(CloudResourceProviderRegistry.class).register(crp.as(CloudResourceProvider.class));
+
+        System.out.println("Registered CRP: " + crp.getUrl());
+
+        return 0;
+    }
+}
